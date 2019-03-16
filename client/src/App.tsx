5fa5935871d8c51b540dba1499d325475aab2d4c
Dillon <dillon.p.kerr@gmail.com>
2019-03-15 22:11:43 -0400

    Front-end App Setup (#5)
    Sets up the front-end application and a corresponding CI pipeline.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return <div className="App">Taps app</div>;
+  }
+}
+
+export default App;
