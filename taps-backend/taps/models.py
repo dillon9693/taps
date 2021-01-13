41a24b5a021f956430aa35464f5b0b55d4650f52
Dillon Kerr <dillon.p.kerr@gmail.com>
2021-01-12 19:26:55 -0500

    Define basic beer and brewery models and generate migrations

--- taps-backend/taps/models.py
+++ taps-backend/taps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Brewery(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Beer(models.Model):
+    name = models.CharField(max_length=100)
+    brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
