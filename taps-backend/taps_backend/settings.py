6f83533b4b000eedcd0a6d3ddaa3dedda7d7ac2d
Dillon Kerr <dillon.p.kerr@gmail.com>
2021-01-12 21:53:29 -0500

    Get an initial GraphQL schema set up

--- taps-backend/taps_backend/settings.py
+++ taps-backend/taps_backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taps'
+    'taps',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': "taps.schema.schema"
+}
