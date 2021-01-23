6e646b5824720f332495ddaf48ded8f6b795cf12
Dillon Kerr <dillon.p.kerr@gmail.com>
2021-01-23 11:44:08 -0500

    Define top tags resolver (#15)
    This PR defines a Graphene resolver for fetching the top N tags as specified in the GraphQL query. By default, N is 10. The "top" is defined as the tag with the most associated beers. This will most likely change in the future and become a more complicated calculation in the future, but is a good starting point.
    
    I also:
    
        Consolidated fixtures into 1 JSON file, so we can run with 1 command
        Create a Tag model
        Regenereated initial migration

--- taps-backend/taps/models.py
+++ taps-backend/taps/models.py
@@ -7,10 +7,21 @@ class Brewery(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Breweries"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 
 class Beer(models.Model):
     name = models.CharField(max_length=100)
     brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name="beers", blank=True)
 
     def __str__(self):
         return self.name
--- taps-backend/taps/schema.py
+++ taps-backend/taps/schema.py
@@ -1,7 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Count
 
-from taps.models import Beer, Brewery
+from taps.models import Beer, Brewery, Tag
 
 
 class BreweryType(DjangoObjectType):
@@ -16,9 +17,16 @@ class BeerType(DjangoObjectType):
         fields = ("id", "name", "brewery")
 
 
+class TagType(DjangoObjectType):
+    class Meta:
+        model = Tag
+        fields = ("id", "name", "beers")
+
+
 class Query(graphene.ObjectType):
     all_beers = graphene.List(BeerType)
     brewery_by_name = graphene.Field(BreweryType, name=graphene.String(required=True))
+    top_tags = graphene.List(TagType, count=graphene.Int(required=False))
 
     def resolve_all_beers(root, info):
         return Beer.objects.select_related("brewery").all()
@@ -28,6 +36,15 @@ class Query(graphene.ObjectType):
             return Brewery.objects.get(name=name)
         except Brewery.DoesNotExist:
             return None
+
+    # Gets top N tags ordered by number of beers associated with the tag
+    def resolve_top_tags(root, info, count=10):
+        # TODO move this query into a module or a class method on the Tag model
+        return (
+            Tag.objects.prefetch_related("beers")
+            .annotate(beer_count=Count("beers"))
+            .order_by("-beer_count", "name")[:count]
+        )
 
 
 schema = graphene.Schema(query=Query)
