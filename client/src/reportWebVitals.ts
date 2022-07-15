9a1427c3a7cc71b2ab7d35438dc27b3b0af9fd13
Dillon Kerr <dillon.p.kerr@gmail.com>
2022-07-14 22:28:37 -0400

    Bad formatting

--- client/src/reportWebVitals.ts
+++ client/src/reportWebVitals.ts
@@ -6,8 +6,7 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getLCP(onPerfEntry);getTTFB(onPerfEntry);
     });
   }
 };
