8acbd910165abbc08b304d7e531efa92d69bdb9e
Dillon Kerr <dillon.p.kerr@gmail.com>
2022-07-16 13:37:53 -0400

    Redirect root route to /home

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -15,6 +15,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
+          <Route index element={<Navigate to="home" replace />} />
           <Route path="home" element={<Home />} />
           <Route path="search" element={<Search />} />
         </Route>
