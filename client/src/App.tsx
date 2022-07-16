962613c84627bac9221b1d7d4e57baf3a383e752
Dillon Kerr <dillon.p.kerr@gmail.com>
2022-07-15 23:52:56 -0400

    Basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/home">Home</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
