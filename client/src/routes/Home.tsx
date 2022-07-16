f8ac837167b1edb01dc3636f5d5b20d4f7524de1
Dillon Kerr <dillon.p.kerr@gmail.com>
2022-07-16 14:24:18 -0400

    Get a list of beers and render them on the home page

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Beer, getBeers } from "../services/beerService";
 
 export default function Home() {
+  const [beers, setBeers] = useState<Beer[]>([]);
+
+  useEffect(() => {
+    getBeers().then((beersResult) => setBeers(beersResult));
+  }, []);
+
   return (
     <main>
       <h2>Home</h2>
+      <ol>
+        {beers.map((beer) => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ol>
     </main>
   );
 }
