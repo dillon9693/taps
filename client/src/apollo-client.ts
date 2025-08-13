67c81739ecf0fae712773c1e706b3757402e2370
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-08-13 12:59:08 -0400

    Pull graph ql endpoint into varA

--- client/src/apollo-client.ts
+++ client/src/apollo-client.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+// TODO generalize this for different environments
+const GRAPHQL_ENDPOINT = 'http://localhost:8000/graphql';
+
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',  // Django GraphQL endpoint (no trailing slash)
+  uri: GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
