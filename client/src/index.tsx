c0fdfaeb34c065b37609fb29f62e928c3ff4a33d
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-09-03 18:16:32 -0400

    Format

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -13,7 +13,7 @@ if (version) {
 }
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
@@ -28,7 +28,7 @@ root.render(
         <Route path="*" element={<Navigate to="home" replace />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
