415b8d1bf7eb73c1746256a56bdce5fe79917611
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-10-01 20:54:41 -0400

    Add downvote / upvote to tag

--- client/src/components/Tag.tsx
+++ client/src/components/Tag.tsx
@@ -1,4 +1,8 @@
-import { Badge, useMantineTheme } from "@mantine/core";
+import { Badge, Button, Text, useMantineTheme } from "@mantine/core";
+import {
+  IconTriangleFilled,
+  IconTriangleInvertedFilled,
+} from "@tabler/icons-react";
 import { Beer, Tag as TagType } from "../types";
 
 interface TagProps {
@@ -9,8 +13,12 @@ interface TagProps {
 export default function Tag({ beer, tag }: TagProps) {
   const theme = useMantineTheme();
 
+  const upvotes = 10;
+  const downvotes = 6;
+
   console.log(beer);
 
+  // TODO stack count under arrow?
   return (
     <Badge
       key={tag.name}
@@ -21,7 +29,27 @@ export default function Tag({ beer, tag }: TagProps) {
         color: theme.colors.accent[5],
       }}
     >
+      <Button
+        p={2}
+        m={2}
+        variant="subtle"
+        onClick={() => console.log("downvote")}
+      >
+        <IconTriangleInvertedFilled size={10} />
+        <Text size="xs">{downvotes}</Text>
+      </Button>
+
       {tag.name}
+
+      <Button
+        p={2}
+        m={2}
+        variant="subtle"
+        onClick={() => console.log("upvote")}
+      >
+        <IconTriangleFilled size={10} />
+        <Text size="xs">{upvotes}</Text>
+      </Button>
     </Badge>
   );
 }
