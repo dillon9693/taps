6223bec6756fc5d3614fcd74032f0dda75412a3f
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-10-06 19:00:10 -0400

    Use Mantine notif system to display error when voting

--- client/src/components/Tag.tsx
+++ client/src/components/Tag.tsx
@@ -5,6 +5,7 @@ import {
   IconTriangleInvertedFilled,
 } from "@tabler/icons-react";
 import { useMutation, useQuery } from "@apollo/client";
+import { notifications } from "@mantine/notifications";
 import type { Beer, TagWithVotes } from "../types";
 import { TAG_VOTE, type TagVoteResult } from "../graphql/mutations";
 import { GET_CURRENT_USER } from "../graphql/queries";
@@ -31,11 +32,7 @@ export default function Tag({ beer, tagWithVotes }: TagProps) {
 
   const isAuthenticated = !currentUserError && !!currentUserData?.currentUser;
 
-  const [voteForTag, { data, loading, error }] =
-    useMutation<TagVoteResult>(TAG_VOTE);
-
-  // TODO how to handle errors?
-  console.log(error);
+  const [voteForTag, { data, loading }] = useMutation<TagVoteResult>(TAG_VOTE);
 
   const upvoteCount = data?.tagVote?.success
     ? data.tagVote.newUpvoteCount
@@ -59,7 +56,13 @@ export default function Tag({ beer, tagWithVotes }: TagProps) {
         },
       });
 
-      if (!result.errors && result.data?.tagVote.success) {
+      if (result.errors || !result.data?.tagVote.success) {
+        notifications.show({
+          title: "Vote failed",
+          message: "Something went wrong while voting. Please try again.",
+          color: "red",
+        });
+      } else {
         setUserVoteType(upvote);
       }
     };
