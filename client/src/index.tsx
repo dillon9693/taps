d3e45a7a7dad0af9cd2027163a71af0799ea19e0
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-10-07 12:59:19 -0400

    Create RequireAuthenticated component to wrap auth-only routes; use for Account route

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -22,6 +22,7 @@ import Login from "./routes/Login";
 import Register from "./routes/Register";
 import RequireUnauthenticated from "./components/RequireUnauthenticated";
 import Account from "./routes/Account";
+import RequireAuthenticated from "./components/RequireAuthenticated";
 
 const version = process.env.REACT_APP_VERSION;
 if (version) {
@@ -49,7 +50,14 @@ root.render(
                 <Route path="search" element={<Search />} />
                 <Route path="beer/:id" element={<BeerDetail />} />
                 <Route path="brewery/:id" element={<BreweryDetail />} />
-                <Route path="account" element={<Account />} />
+                <Route
+                  path="account"
+                  element={
+                    <RequireAuthenticated>
+                      <Account />
+                    </RequireAuthenticated>
+                  }
+                />
 
                 {/* Start auth routes */}
                 <Route
