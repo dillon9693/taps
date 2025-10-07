8768e80f8833e3f2c8b2def375cc865a389d81d1
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-10-06 23:03:57 -0400

    Fix tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,16 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
+import { MockedProvider } from "@apollo/client/testing";
 import App from "./App";
 import { theme } from "./theme/theme";
+import { AuthProvider } from "./contexts/AuthContext";
+import { GET_CURRENT_USER } from "./graphql/queries";
+
+const mocks = [
+  {
+    request: {
+      query: GET_CURRENT_USER,
+    },
+    result: {
+      data: {
+        currentUser: null,
+      },
+    },
+  },
+];
 
 test("renders app with navigation", () => {
   render(
-    <MantineProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </MantineProvider>,
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <AuthProvider>
+        <MantineProvider theme={theme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </MantineProvider>
+      </AuthProvider>
+    </MockedProvider>,
   );
 
   // Test that the app renders with navigation links
