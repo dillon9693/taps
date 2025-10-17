550b0359666d23698b9d6bd700d75c0da053dbc2
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-10-17 15:15:42 -0400

    Support other states

--- taps-backend/taps/management/commands/import_brewery_data.py
+++ taps-backend/taps/management/commands/import_brewery_data.py
@@ -7,17 +7,86 @@ from django.core.management.base import BaseCommand
 
 from taps.models import Brewery, BrewerySource
 
-MA_URL = "https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/refs/heads/master/data/united-states/massachusetts.csv"
+OBDB_USA_URL_PREFIX = "https://raw.githubusercontent.com/openbrewerydb/openbrewerydb/refs/heads/master/data/united-states"
+
+STATE_ABBR_TO_FILENAME = {
+    "AL": "alabama.csv",
+    "AK": "alaska.csv",
+    "AZ": "arizona.csv",
+    "AR": "arkansas.csv",
+    "CA": "california.csv",
+    "CO": "colorado.csv",
+    "CT": "connecticut.csv",
+    "DE": "delaware.csv",
+    "DC": "district-of-columbia.csv",
+    "FL": "florida.csv",
+    "GA": "georgia.csv",
+    "HI": "hawaii.csv",
+    "ID": "idaho.csv",
+    "IL": "illinois.csv",
+    "IN": "indiana.csv",
+    "IA": "iowa.csv",
+    "KS": "kansas.csv",
+    "KY": "kentucky.csv",
+    "LA": "louisiana.csv",
+    "ME": "maine.csv",
+    "MD": "maryland.csv",
+    "MA": "massachusetts.csv",
+    "MI": "michigan.csv",
+    "MN": "minnesota.csv",
+    "MS": "mississippi.csv",
+    "MO": "missouri.csv",
+    "MT": "montana.csv",
+    "NE": "nebraska.csv",
+    "NV": "nevada.csv",
+    "NH": "new-hampshire.csv",
+    "NJ": "new-jersey.csv",
+    "NM": "new-mexico.csv",
+    "NY": "new-york.csv",
+    "NC": "north-carolina.csv",
+    "ND": "north-dakota.csv",
+    "OH": "ohio.csv",
+    "OK": "oklahoma.csv",
+    "OR": "oregon.csv",
+    "PA": "pennsylvania.csv",
+    "RI": "rhode-island.csv",
+    "SC": "south-carolina.csv",
+    "SD": "south-dakota.csv",
+    "TN": "tennessee.csv",
+    "TX": "texas.csv",
+    "UT": "utah.csv",
+    "VT": "vermont.csv",
+    "VA": "virginia.csv",
+    "WA": "washington.csv",
+    "WV": "west-virginia.csv",
+    "WI": "wisconsin.csv",
+    "WY": "wyoming.csv",
+}
 
 
 class Command(BaseCommand):
     help = "Imports brewery data from OpenBreweryDB"
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Importing data!")
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--state",
+            required=True,
+            choices=STATE_ABBR_TO_FILENAME.keys(),
+            help="State to import data for",
+        )
 
-        # TODO arguments for specific data (or all data)
-        contents = urllib.request.urlopen(MA_URL).read().decode("utf-8")
+    def handle(self, *args, **options):
+        state = options["state"]
+        obdb_filename = STATE_ABBR_TO_FILENAME.get(state)
+        if not obdb_filename:
+            self.stderr.write("Invalid state code passed")
+            return
+
+        import_url = f"{OBDB_USA_URL_PREFIX}/{obdb_filename}"
+
+        self.stdout.write(f"\nStarting data import for {state} from {import_url}!\n")
+
+        contents = urllib.request.urlopen(import_url).read().decode("utf-8")
         total_count = contents.count("\n") - 1
 
         reader = csv.DictReader(io.StringIO(contents))
