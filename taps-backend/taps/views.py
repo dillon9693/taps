f4c00b9236de31e193375ffad71b5a38e4083e77
Dillon Kerr <dillon.p.kerr@gmail.com>
2025-11-17 12:53:32 -0500

    Revert "test: intentionally break type checking"
    This reverts commit a44d5e7c3e16b2b0a4b467ef31e2afc39fd7a8d0.

--- taps-backend/taps/views.py
+++ taps-backend/taps/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-def index(request: HttpRequest) -> int:
+def index(request: HttpRequest) -> HttpResponse:
     """
     Index view.
     """
