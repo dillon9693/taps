f55ff06ed74a5d0ee67c22a91d4e1778fb2c722a
Dillon Kerr <dillon.p.kerr@gmail.com>
2026-02-23 13:25:20 -0500

    Implement form on home page

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,12 +1,49 @@
-import { Container, Title } from "@mantine/core";
+import {
+  Button,
+  Center,
+  Container,
+  Grid,
+  Group,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 export default function Home() {
+  const queryForm = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      query: "",
+    },
+  });
+
+  const onQueryFormSubmit = queryForm.onSubmit((values) => {
+    console.log(values);
+    // TODO send request
+  });
+
   return (
     <Container mt="xl">
-      <Title order={1} mb="lg">
-        Home!
-      </Title>
-      Coming Soon!
+      <Center>
+        <Title order={1} mb="lg">
+          Cheers to you! 🍻
+        </Title>
+      </Center>
+
+      <form onSubmit={onQueryFormSubmit}>
+        <Grid gutter="lg">
+          <Grid.Col span={12}>
+            <TextInput
+              label="What's going on today?"
+              {...queryForm.getInputProps("query")}
+            />
+
+            <Group justify="flex-end" mt="md">
+              <Button type="submit">Find some beers!</Button>
+            </Group>
+          </Grid.Col>
+        </Grid>
+      </form>
     </Container>
   );
 }
